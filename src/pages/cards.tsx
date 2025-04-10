021d198458bac719ad2a292f5d487113d98e0d74
Keymor <vlad.kutuzov96@gmail.com>
2025-04-10 18:53:55 +0700

    deploy

--- src/pages/cards.tsx
+++ src/pages/cards.tsx
@@ -47,7 +47,7 @@ export default function Cards() {
     const hendleLessonNum = (lesson: string) => {
         const lastChar = lesson.charAt(lesson.length - 1)
         const lessonNumber = parseInt(lastChar)
-        setLessonNum((lesson) => lesson = lessonNumber)
+        setLessonNum(lessonNumber)
     }
 
     const finishLessonFun = () => {
